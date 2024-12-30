d13c490efb4dc9bff9640ea8b89dbf54e9ba0bd9
Filip Rojek <filip@filiprojek.cz>
2024-12-30 14:35:16 +0100

    Added: Habits dashboard

--- app/views/habits/index.php
+++ app/views/habits/index.php
@@ -3,33 +3,22 @@
     <?php if (empty($this->get('habits'))): ?>
         <p>No habits yet. <a href="/habits/create">Create your first habit</a>.</p>
     <?php else: ?>
-        <table>
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Frequency</th>
-                    <th>Custom Schedule</th>
-                    <th>Points</th>
-                    <th>Created At</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($this->get('habits') as $habit): ?>
-                    <tr>
-                        <td><?= htmlspecialchars($habit['title']) ?></td>
-                        <td><?= htmlspecialchars($habit['frequency']) ?></td>
-                        <td><?= htmlspecialchars($habit['custom_frequency'] ?? 'N/A') ?></td>
-                        <td><?= htmlspecialchars($habit['reward_points']) ?></td>
-                        <td><?= htmlspecialchars($habit['created_at']) ?></td>
-                        <td>
-                            <a href="/habits/edit?id=<?= $habit['id'] ?>">Edit</a> |
-                            <a href="/habits/delete?id=<?= $habit['id'] ?>" onclick="return confirm('Are you sure you want to delete this habit?')">Delete</a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-        <a href="/habits/create">Create new habit!</a>
+        <div class="habits-wrapper">
+        <?php foreach ($this->get('habits') as $habit): ?>
+            <div class="habit bordered">
+                <b><?= htmlspecialchars($habit['title']) ?></b>
+                <p>Frequency: <?= htmlspecialchars($habit['frequency']) ?></p>
+                <?php if (isset($habit['custom_frequency'])): ?>
+                    <p><?= htmlspecialchars($habit['custom_frequency'] ?? 'N/A') ?></p>
+                <?php endif; ?>
+                    <p><?= htmlspecialchars($habit['reward_points']) ?></p>
+                    <p><?= htmlspecialchars($habit['created_at']) ?></p>
+                    <a href="/habits/done">Mark as done</a> |
+                    <a href="/habits/edit?id=<?= $habit['id'] ?>">Edit</a> |
+                    <a href="/habits/delete?id=<?= $habit['id'] ?>" onclick="return confirm('Are you sure you want to delete this habit?')">Delete</a>
+            </div>
+        <?php endforeach; ?>
+        </div>
+        <a href="/habits/create" class="btn-green">Create new habit!</a>
     <?php endif; ?>
 </section>
